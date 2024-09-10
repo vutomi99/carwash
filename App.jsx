7acad259fa82abe91c6ad3becc5141108e5e4f36
vutomi99 <vutomi99.ngobeni@gmail.com>
2024-09-11 01:48:06 +0200

    initial commit

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+     <Stack.Navigator>
+     <Stack.Screen name="Home" component={HomeScreen} />
+     </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
